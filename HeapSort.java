f9148881a9b9c5f517804dffc08faa03ce3dd36f
ThickerSiren <47922362+ThickerSiren@users.noreply.github.com>
2020-06-21 12:11:15 -0500

    Add files via upload

---
+++ HeapSort.java
@@ -0,0 +1,18 @@
+
+public class HeapSort {
+
+	public static <E extends Comparable<E>> void inOrderSort(E[] list)
+	{
+		Heap<E> heap = new Heap<>();
+		// Adding elements to the heap
+		
+		for( int i=0; i< list.length; i++)
+			heap.add(list[i]);
+		
+		// Removing elements from the heap
+		
+		for(int i=0; i<list.length; i++)
+			list[i]= heap.remove();
+	}
+	
+}
